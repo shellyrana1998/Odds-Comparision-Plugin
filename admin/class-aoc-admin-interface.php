ff410228607d1a6a422ab75a349834e079ffeba8
shellyrana1998 <ranaprologics@gmail.com>
2025-05-23 07:13:47 +0530

    Update class-aoc-admin-interface.php

--- admin/class-aoc-admin-interface.php
+++ admin/class-aoc-admin-interface.php
@@ -1,11 +1,13 @@
 <?php
 class AOC_Admin_Interface {
+    
     public function __construct() {
-         // Hook into WordPress admin menu and init actions
+        // Hook into WordPress admin menu and init actions
         add_action('admin_menu', [$this, 'create_menu']);
         add_action('admin_init', [$this, 'register_settings']);
     }
-   // Creates a new admin menu item for Odds Settings
+
+    // Creates a new admin menu item for Odds Settings
     public function create_menu() {
         add_menu_page(
             'Odds Settings',            // Page title
@@ -15,15 +17,16 @@ class AOC_Admin_Interface {
             [$this, 'settings_page']    // Callback to render the settings page
         );
     }
-    
+
     // Registers settings so they can be saved via options.php
     public function register_settings() {
         register_setting('aoc-settings-group', 'aoc_api_key');
-    register_setting('aoc-settings-group', 'aoc_region');
+        register_setting('aoc-settings-group', 'aoc_region');
         register_setting('aoc-settings-group', 'aoc_selected_bookmakers');
         register_setting('aoc-settings-group', 'aoc_selected_markets');
         register_setting('aoc-settings-group', 'aoc_bookmaker_links');
     }
+
     // Renders the admin settings page UI
     public function settings_page() {
         // Retrieve stored option values or use defaults
@@ -40,51 +43,76 @@ class AOC_Admin_Interface {
             <h1>Odds Comparison Settings</h1>
             <form method="post" action="options.php">
                 <?php
-                settings_fields('aoc-settings-group');
-                do_settings_sections('aoc-settings-group');
+                settings_fields('aoc-settings-group');   // Output security fields for the settings form
+                do_settings_sections('aoc-settings-group'); // Output registered sections (none used here)
                 ?>
                 <table class="form-table">
-        <tr>
-            <th scope="row">Select Region</th>
-            <td>
-                <select name="aoc_region">
-                    <option value="us" <?php selected(get_option('aoc_region', 'us'), 'us'); ?>>United States (US)</option>
-                    <option value="uk" <?php selected(get_option('aoc_region'), 'uk'); ?>>United Kingdom (UK)</option>
-                    <option value="eu" <?php selected(get_option('aoc_region'), 'eu'); ?>>Europe (EU)</option>
-                    <option value="au" <?php selected(get_option('aoc_region'), 'au'); ?>>Australia (AU)</option>
-                </select>
-            </td>
-        </tr>
+
+                    <!-- Region Dropdown -->
+                    <tr>
+                        <th scope="row">Select Region</th>
+                        <td>
+                            <select name="aoc_region">
+                                <option value="us" <?php selected(get_option('aoc_region', 'us'), 'us'); ?>>United States (US)</option>
+                                <option value="uk" <?php selected(get_option('aoc_region'), 'uk'); ?>>United Kingdom (UK)</option>
+                                <option value="eu" <?php selected(get_option('aoc_region'), 'eu'); ?>>Europe (EU)</option>
+                                <option value="au" <?php selected(get_option('aoc_region'), 'au'); ?>>Australia (AU)</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <!-- API Key Field -->
                     <tr>
                         <th scope="row">API Key</th>
-                        <td><input type="text" name="aoc_api_key" value="<?= esc_attr($api_key) ?>" class="regular-text"/></td>
+                        <td>
+                            <input type="text" name="aoc_api_key" value="<?= esc_attr($api_key) ?>" class="regular-text"/>
+                        </td>
                     </tr>
+
+                    <!-- Bookmaker Checkbox List -->
                     <tr>
                         <th scope="row">Select Bookmakers</th>
                         <td>
                             <?php foreach ($bookmakers as $bookmaker): ?>
-                                <label><input type="checkbox" name="aoc_selected_bookmakers[]" value="<?= $bookmaker ?>" <?= in_array($bookmaker, $selected_bookmakers) ? 'checked' : '' ?>/> <?= $bookmaker ?></label><br/>
+                                <label>
+                                    <input type="checkbox" name="aoc_selected_bookmakers[]" value="<?= $bookmaker ?>" 
+                                    <?= in_array($bookmaker, $selected_bookmakers) ? 'checked' : '' ?>/>
+                                    <?= $bookmaker ?>
+                                </label><br/>
                             <?php endforeach; ?>
                         </td>
                     </tr>
+
+                    <!-- Market Checkbox List -->
                     <tr>
                         <th scope="row">Select Markets</th>
                         <td>
                             <?php foreach ($markets as $market): ?>
-                                <label><input type="checkbox" name="aoc_selected_markets[]" value="<?= $market ?>" <?= in_array($market, $selected_markets) ? 'checked' : '' ?>/> <?= $market ?></label><br/>
+                                <label>
+                                    <input type="checkbox" name="aoc_selected_markets[]" value="<?= $market ?>" 
+                                    <?= in_array($market, $selected_markets) ? 'checked' : '' ?>/>
+                                    <?= $market ?>
+                                </label><br/>
                             <?php endforeach; ?>
                         </td>
                     </tr>
+
+                    <!-- Bookmaker Link Inputs -->
                     <tr>
                         <th scope="row">Bookmaker Links</th>
                         <td>
                             <?php foreach ($bookmakers as $bookmaker): ?>
-                                <label><?= $bookmaker ?> Link: <input type="text" name="aoc_bookmaker_links[<?= $bookmaker ?>]" value="<?= esc_attr($bookmaker_links[$bookmaker] ?? '') ?>" class="regular-text"/></label><br/>
+                                <label>
+                                    <?= $bookmaker ?> Link:
+                                    <input type="text" name="aoc_bookmaker_links[<?= $bookmaker ?>]" 
+                                    value="<?= esc_attr($bookmaker_links[$bookmaker] ?? '') ?>" class="regular-text"/>
+                                </label><br/>
                             <?php endforeach; ?>
                         </td>
                     </tr>
+
                 </table>
-                <?php submit_button(); ?>
+                <?php submit_button(); // Renders a "Save Changes" button ?>
             </form>
         </div>
         <?php
