61e72b9ddb80dcb235a92bb98d3e7f94b418b893
shellyrana1998 <ranaprologics@gmail.com>
2025-05-23 06:24:11 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,29 +5,36 @@
  * Version: 1.0
  * Author: Shelly Rana
  */
-
+// This stops the file from running directly (for security)
 if (!defined('ABSPATH')) exit;
 
+// These files help in creating admin settings and fetching data from the API
 require_once plugin_dir_path(__FILE__) . 'admin/class-aoc-admin-interface.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-aoc-scraper.php';
 require_once plugin_dir_path(__FILE__) . 'includes/class-aoc-odds-converter.php';
 
+// Create the admin settings page
 new AOC_Admin_Interface();
 
+// This function registers the Gutenberg block so it appears in the editor
 function aoc_register_block() {
     wp_register_script(
         'aoc-odds-block',
         plugins_url('blocks/odds-block.js', __FILE__),
-        array('wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-data'),
-        filemtime(plugin_dir_path(__FILE__) . 'blocks/odds-block.js')
+        array('wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-data'), // Needed WordPress scripts
+        filemtime(plugin_dir_path(__FILE__) . 'blocks/odds-block.js') // This makes sure browser gets the latest file
     );
-
+    );
+    // Register the block so we can use it in editor and frontend
     register_block_type('aoc/odds-comparison', array(
-        'editor_script' => 'aoc-odds-block',
-        'render_callback' => 'aoc_render_odds_block'
+        'editor_script' => 'aoc-odds-block', // Script used in the editor
+        'render_callback' => 'aoc_render_odds_block' // Function that shows data on the website
     ));
 }
 add_action('init', 'aoc_register_block');
+
+
+// Load the block's editor script when editing a post or page
 add_action('enqueue_block_editor_assets', 'aoc_editor_assets');
 function aoc_editor_assets() {
     wp_enqueue_script(
@@ -38,6 +45,7 @@ function aoc_editor_assets() {
     );
 }
 
+// This function creates the odds table on the frontend of the website
 function aoc_render_odds_block($attributes) {
     $odds = AOC_Scraper::fetch_odds();
     ob_start();
@@ -61,7 +69,7 @@ function aoc_render_odds_block($attributes) {
     echo '<div class="aoc-odds-table">';
     echo '<table><thead><tr><th>Bookmaker</th><th>Market</th><th>Team</th><th>Odds</th><th>Link</th></tr></thead><tbody>';
 
-    // Loop through each fetched odd entry
+    // Loop through each fetched odd entry  // Show each row if we have odds
     if (!empty($odds)) {
         foreach ($odds as $entry) {
             echo '<tr>';
@@ -80,10 +88,11 @@ function aoc_render_odds_block($attributes) {
             echo '</tr>';
         }
     } else {
+        // If no odds found, show this message
         echo '<tr><td colspan="5" style="text-align:center;color:red;">No matching odds found for selected filters.</td></tr>';
     }
 
     echo '</tbody></table></div>';
 
-    return ob_get_clean();
+    return ob_get_clean(); // Send the HTML to show on the site
 }
