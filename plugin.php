8e2f602f511fdb9a569ee8a37761aebf50169951
shellyrana1998 <ranaprologics@gmail.com>
2025-05-23 07:10:40 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,6 +5,7 @@
  * Version: 1.0
  * Author: Shelly Rana
  */
+
 // This stops the file from running directly (for security)
 if (!defined('ABSPATH')) exit;
 
@@ -19,12 +20,12 @@ new AOC_Admin_Interface();
 // This function registers the Gutenberg block so it appears in the editor
 function aoc_register_block() {
     wp_register_script(
-        'aoc-odds-block',
-        plugins_url('blocks/odds-block.js', __FILE__),
+        'aoc-odds-block', // Unique name for the script
+        plugins_url('blocks/odds-block.js', __FILE__), // Path to the JavaScript file
         array('wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-data'), // Needed WordPress scripts
         filemtime(plugin_dir_path(__FILE__) . 'blocks/odds-block.js') // This makes sure browser gets the latest file
     );
-    );
+
     // Register the block so we can use it in editor and frontend
     register_block_type('aoc/odds-comparison', array(
         'editor_script' => 'aoc-odds-block', // Script used in the editor
@@ -32,7 +33,6 @@ function aoc_register_block() {
     ));
 }
 add_action('init', 'aoc_register_block');
-
 
 // Load the block's editor script when editing a post or page
 add_action('enqueue_block_editor_assets', 'aoc_editor_assets');
@@ -47,9 +47,10 @@ function aoc_editor_assets() {
 
 // This function creates the odds table on the frontend of the website
 function aoc_render_odds_block($attributes) {
-    $odds = AOC_Scraper::fetch_odds();
-    ob_start();
+    $odds = AOC_Scraper::fetch_odds(); // Get odds from API
+    ob_start(); // Start storing HTML output
 
+    // Some simple styles for the table
     echo '<style>
         .aoc-odds-table table {
             width: 100%;
@@ -69,16 +70,16 @@ function aoc_render_odds_block($attributes) {
     echo '<div class="aoc-odds-table">';
     echo '<table><thead><tr><th>Bookmaker</th><th>Market</th><th>Team</th><th>Odds</th><th>Link</th></tr></thead><tbody>';
 
-    // Loop through each fetched odd entry  // Show each row if we have odds
+    // Show each row if we have odds
     if (!empty($odds)) {
         foreach ($odds as $entry) {
             echo '<tr>';
-            echo '<td>' . esc_html($entry['bookmaker']) . '</td>';
-            echo '<td>' . esc_html($entry['market']) . '</td>';
-            echo '<td>' . esc_html($entry['team']) . '</td>';
-            echo '<td>' . esc_html($entry['odds']) . '</td>';
+            echo '<td>' . esc_html($entry['bookmaker']) . '</td>'; // Bookmaker name
+            echo '<td>' . esc_html($entry['market']) . '</td>'; // Market name
+            echo '<td>' . esc_html($entry['team']) . '</td>'; // Team name
+            echo '<td>' . esc_html($entry['odds']) . '</td>'; // Odds number
 
-            // Only show link if it's not empty
+            // Show link if it's there, otherwise show a dash
             if (!empty($entry['link']) && $entry['link'] !== '#') {
                 echo '<td><a href="' . esc_url($entry['link']) . '" target="_blank">Visit</a></td>';
             } else {
